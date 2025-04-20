097acc1d37424c80e12ce00414bffcbdd308a8d8
harshk-eyezense <harsh.kumar@eyezense.com>
2025-04-20 14:57:08 +0530

    Integrated PostgreSQL with FastAPI CRUD project

---
+++ src/books/models.py
@@ -0,0 +1,31 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+from datetime import datetime
+import uuid
+
+
+class Book(SQLModel, table=True):
+    __tablename__ = "books"
+
+    uid: uuid.UUID = Field(
+        sa_column= Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4()
+        )
+    )
+    
+    author: str
+    country: str
+    imageLink: str
+    language: str
+    link: str
+    pages: int
+    title: str
+    year: int
+    created_at : datetime = Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now))
+    updated_at : datetime = Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now))
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
